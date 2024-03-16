aa063b7dceaf29b2999dbefa07d95a27ddf26b97
fkdkskdfjfk <yljw225@naver.com>
2024-03-16 16:20:21 +0900

    몽고디비 웹서버용

--- database/index.js
+++ database/index.js
@@ -1,15 +1,19 @@
 const { MongoClient } = require('mongodb');
 
 const { MONGO_ID, MONGO_PASSWORD } = process.env;
-const url = `mongodb+srv://${MONGO_ID}:${MONGO_PASSWORD}@cluster0.hqitiuj.mongodb.net/`;
+// const url = `mongodb+srv://${MONGO_ID}:${MONGO_PASSWORD}@cluster0.hqitiuj.mongodb.net/`;
+const uri = 'mongodb://localhost:27017'
 const client = new MongoClient(url);
 
 const connect = async () => {
     try {
       await client.connect(); 
-      console.log('몽고디비 연결 성공!');
+      console.log('Connected to MongoDB!');
+      await db.authenticate(MONGO_ID, MONGO_PASSWORD);
+
+      return db;
     } catch (err) {
-      console.error('몽고디비 연결 에러', err);
+      console.error('mongoDB connect error', err);
     }
   };
   
